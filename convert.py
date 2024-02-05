e5b685a891edd382e113557cfac51d3be231581c
LukesFather2011 <lukesfather0826@gmail.com>
2024-02-04 21:20:48 -0900

    create convert.py and morse.py

---
+++ convert.py
@@ -0,0 +1,10 @@
+from morse import Morse
+
+
+class Converter:
+
+    def text_to_morse(self):
+        pass
+
+    def morse_to_text(self):
+        pass
--- main.py
+++ main.py
@@ -1,31 +1,18 @@
+# This program takes user input and converts it into Morse code and vice versa.
+from morse import Morse
+
+# TODO 1: Create functions to convert text to morse, and morse to text.
 
 
-MORSE_DICT = {
-    'a': '. _',
-    'b': '_ . . .',
-    'c': '_ . _ .',
-    'd': '_ . .',
-    'e': '.',
-    'f': '. . _ .',
-    'g': '_ _ .',
-    'h': '. . . .',
-    'i': '. .',
-    'j': '. _ _ _',
-    'k': '_ . _',
-    'l': '. _ . . ',
-    'm': '_ _',
-    'n': '_ .',
-    'o': '_ _ _',
-    'p': '. _ _ .',
-    'q': '_ _ . _',
-    'r': '. _ .',
-    's': '. . .',
-    't': '_',
-    'u': '. . _',
-    'v': '. . . _',
-    'w': '. _ _',
-    'x': '_ . . _',
-    'y': '_ . _ _',
-    'z': '_ _ . .'
-}
+# This is the welcome statement presented to the user upon running the program.
+print("Hello, welcome to text-to-morse code converter")
 
+# This loop determines if the user wants to run the program again. If False, it terminates.
+again = True
+while again:
+
+    user_text = input("Please enter what you would like to convert:\n")
+    morse_code_conversion = ""
+    for i in user_text:
+        morse_code_conversion += i
+    print(morse_code_conversion)

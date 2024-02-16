5e6ab721e5e1355f7339d8775a10e88b49180a23
lukesfather2011 <lukesfather0826@gmail.com>
2024-02-16 12:30:07 -0900

    Update main.py with main.loop()

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 # This program takes user input and converts it into Morse code and vice versa.
 from convert import text_to_morse
 
-# This is the welcome statement presented to the user upon running the program.
+
+def main_loop():
+    """Runs the main loop of the program."""
+    # Variable to manage the while loop.
+    run_again = True
+
+    while run_again:
+        text_to_convert = input("Please enter the text you would like to convert:\n")
+        # Refer to convert.py. Pulls morse characters from morse.py
+        print(text_to_morse(text_to_convert))
+
+        def loop_again():
+            # runs main.loop again if "y", terminates if "n", and runs the inquiry again if not a valid character.
+            answer = input("Would you like to convert another string? y or n:").lower()
+            if answer == "y":
+                main_loop()
+            elif answer == "n":
+                print("Have a great day! Bye!")
+                return False
+            else:
+                print("Please enter a valid response.")
+                loop_again()
+
+        run_again = loop_again()
+
+
+# This is the welcome statement presented to the user upon running the program, followed by the main.loop.
 print("Hello, welcome to text-to-morse code converter")
+main_loop()
 
-# This loop determines if the user wants to run the program again. If False, it terminates.
-again = True
-while again:
 
-    user_text = input("Please enter what you would like to convert:\n")
-    print(text_to_morse(user_text))
-
-    print("\n")
-    if input("Would you like to convert more text? y or n: ").lower() == 'n':
-        again = False
